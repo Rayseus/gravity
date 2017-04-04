071c09768b693ae9e22f97db5469a2df6f3d4a85
Rayseus <lesoleillry@icloud.com>
2017-04-04 14:30:45 -0300

    Update GravityConstant.java

--- src/main/java/dal/gravity/GravityConstant.java
+++ src/main/java/dal/gravity/GravityConstant.java
@@ -2,7 +2,7 @@ package dal.gravity;
 
 public class GravityConstant implements GravityModel{
 	
-	public static final double EARTH_GRAVITY = 9.80665;
+	public static final double EARTH_GRAVITY = 9.81;
     public static final double JUPITER_GRAVITY = 29.79;
 
     private double gravity;
