cc06a965bd3827a6895cb56fd4a5b4d4d48f42c5
raysonli <raysonli@t82b0.wpa.dal.ca>
2017-04-04 14:07:45 -0300

    Second refactoring with strategy design pattern

---
+++ src/main/java/dal/gravity/GravityConstant.java
@@ -0,0 +1,19 @@
+package dal.gravity;
+
+public class GravityConstant implements GravityModel{
+	
+	public static final double EARTH_GRAVITY = 9.80665;
+    public static final double JUPITER_GRAVITY = 29.79;
+
+    private double gravity;
+
+    public GravityConstant(double gravity) {
+        this.gravity = gravity;
+    }
+
+    @Override
+    public double getGravitationalField() {
+        return gravity;
+    }
+
+}
